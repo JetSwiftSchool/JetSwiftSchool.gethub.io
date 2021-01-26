b600356545125182a5e4f5ca968406383ba92584
school-jet <78052495+school-jet@users.noreply.github.com>
2021-01-26 14:16:14 -0800

    uploaded fact finder for the first time

---
+++ factfinder/script.js
@@ -0,0 +1,25 @@
+document.querySelector("#buttonform").btn1.addEventListener("click", fact1);
+document.querySelector("#buttonform").btn2.addEventListener("click", fact2);
+document.querySelector("#buttonform").btn3.addEventListener("click", fact3);
+document.querySelector("#buttonform").btn4.addEventListener("click", fact4);
+document.querySelector("#buttonform").btn5.addEventListener("click", fact5);
+ 
+function fact1(){
+	document.querySelector("#factOutput").innerHTML = "Some giant tarantulas keep tiny frogs as pets. The tiny frogs help by eating any small bugs that are attacking the tarantula in return the tarantula defend the frog from predators  <br> <img src='images/frog.jpg' alt='image of small frog'>";
+}
+ 
+function fact2(){
+	document.querySelector("#factOutput").innerHTML = "Figure skating is called figure skating because the sport originally was a competition on how well you could draw shape in the ice with your skates <br> <img src='images/skate.jpg' alt='image of a skating rink'>";
+}
+ 
+function fact3(){
+	document.querySelector("#factOutput").innerHTML = "If you stack every cat in the world on top of each other there they wouldn't like it <br> <img src='images/cat.jpg' alt='image of alot of cats'>";
+}
+ 
+function fact4(){
+	document.querySelector("#factOutput").innerHTML = "Breakdancer is an incorrect term that was created by the media, the correct things to call someone is breaker or bboy/bgirl <br> <img src='images/breaker.jpg' alt='image of a bboy in a stale'>";
+}
+ 
+function fact5(){
+	document.querySelector("#factOutput").innerHTML = "There is at least 4 people in this photo <br> <img src='images/earth.jpg' alt='image of earth'>";
+}
